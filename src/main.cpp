0bbf12da492ec60b3bc07ce135647fa5316caa30
jho <jonathanjho03@gmail.com>
2025-06-15 03:53:08 -0700

    CPU registers

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
-#include <cstdint>
-#include <iostream>
 #include <array>
-#include <numeric>
+#include <cstdint>
+#include <cstdio>
+
+union RegisterPair {
+  struct {
+    uint8_t low;
+    uint8_t high;
+  };
+
+  uint16_t value;
+};
+
+struct Registers {
+  RegisterPair af; // high = a, low = f
+  RegisterPair bc;
+  RegisterPair de;
+  RegisterPair hl;
+  uint16_t sp; // stack pointer
+  uint16_t pc; // program counter
+};
+
+class CPU {
+public:
+  std::array<uint8_t, 0xFFFF> memoryMap;
+  Registers registers;
+};
 
 int main() {
-  std::array<uint8_t, 0xFFFF> memoryMap;
-  memoryMap.fill(1);
-  auto bignum = std::accumulate(memoryMap.begin(), memoryMap.end(), 1);
+  CPU cpu;
+  cpu.registers.af.value = 0x1234;
 
-  std::cout << "Sum is: " << bignum << std::endl;
+  printf("A = %02X, F = %02X\n", cpu.registers.af.high,
+         cpu.registers.af.low); // A = 12, F = 34
 
   return 0;
 }
